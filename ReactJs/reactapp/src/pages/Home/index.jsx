7a933185547b5a1e3db665b4471ddbc63264ee68
Isaias-Tortorele <isaiastortorele23@gmail.com>
2022-09-06 18:38:34 -0300

    Estilizando Página CSS

--- ReactJs/reactapp/src/pages/Home/index.jsx
+++ ReactJs/reactapp/src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import './styles.css';
 
 export function Home() {
   return (
-    <div>
+    <div className='container'>
       <h1 >Lista de presença</h1>
       <input type="text" placeholder="Digite seu nome..." />
       <button type="button">Adicionar</button>
