6d46d59aa758752a3c1f176022691edd8467a8e7
Isaias-Tortorele <isaiastortorele23@gmail.com>
2022-09-09 15:32:40 -0300

    Propriedades

--- ReactJs/reactapp/src/pages/Home/index.jsx
+++ ReactJs/reactapp/src/pages/Home/index.jsx
@@ -9,7 +9,8 @@ export function Home() {
       <input type="text" placeholder="Digite seu nome..." />
       <button type="button">Adicionar</button>
 
-      <Card />
+      <Card name="Rodrigo" time="10:55:25" />
+      <Card name="Isaias" time="11:57:20" />
     </div>
   )
 }
