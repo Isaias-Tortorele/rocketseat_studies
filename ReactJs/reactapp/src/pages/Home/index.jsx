8faab923de534f0eafddb8e09afae343119675fc
Isaias-Tortorele <isaiastortorele23@gmail.com>
2022-09-09 15:44:22 -0300

    Estado

--- ReactJs/reactapp/src/pages/Home/index.jsx
+++ ReactJs/reactapp/src/pages/Home/index.jsx
@@ -1,12 +1,19 @@
+import React, { useState } from 'react'
 import './styles.css';
 
 import { Card } from '../../components/card';
 
 export function Home() {
+  const [studentName, setStudentName] = useState();
+
   return (
     <div className='container'>
       <h1 >Lista de presença</h1>
-      <input type="text" placeholder="Digite seu nome..." />
+      <input
+        type="text"
+        placeholder="Digite seu nome..."
+        onChange={e => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Rodrigo" time="10:55:25" />
